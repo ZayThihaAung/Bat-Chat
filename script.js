4a71b0af8ca0551cb552c0357f1026ecf6686507
ZayThiha <109571154+ZayThihaAung@users.noreply.github.com>
2024-03-23 14:08:32 +0630

    Added Hamburger Menu and fixed bugs

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const hamburgerMenuContainerTag = document.querySelector(
-    ".hamburgerMenuContainer"
+  ".hamburgerMenuContainer"
 );
-const hamburgerLine1Tag = document.querySelector(".line1");
-const hamburgerLine2Tag = document.querySelector(".line2");
-const hamburgerLine3Tag = document.querySelector(".line3");
+const hamburgerTag1 = document.querySelector(".hamburger1");
+const hamburgerTag2 = document.querySelector(".hamburger2");
+const hamburgerTag3 = document.querySelector(".hamburger3");
+const compound = [hamburgerTag1, hamburgerTag2, hamburgerTag3];
+const hamburgerItems = document.querySelector('.hamburgerItems');
 
+let index = true;
 hamburgerMenuContainerTag.addEventListener("click", ()=> {
-    hamburgerMenuContainerTag.append(hamburgerLine1Tag, hamburgerLine2Tag, hamburgerLine3Tag);
+  if (index) {
+    hamburgerTag1.style.transform = 'translateY(9px) rotate(45deg)';
+    hamburgerTag2.style.opacity = '0';
+    hamburgerTag3.style.transform = 'translateY(-9px) rotate(-45deg)';
+    for (let i = 0; i < compound.length; i++) {
+      compound[i].classList.add('transForm');
+    }
+    hamburgerItems.style.transform = 'translateY(0cm)';
+    index = false;
+  } else {
+    hamburgerTag1.style.transform = "translateY(0px) rotate(0deg)";
+    hamburgerTag2.style.opacity = '1';
+    hamburgerTag3.style.transform = "translateY(0px) rotate(0deg)";
+    hamburgerItems.style.transform = 'translateY(-18cm)';
+    index = true;
+  }
 })
 
-console.log("Hi");
